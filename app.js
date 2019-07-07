7faa338abe21f6be4f8f44bd7648bee28cbf7f24
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-07-07 15:14:55 -0500

    Included Procfile

--- app.js
+++ app.js
@@ -19,22 +19,15 @@ var usersRouter = require('./routes/users');
 app.use(express.json());
 app.use(cors())
 app.use(bodyParser.json());
+app.use(cors({credentials: true, origin: 'https://storeappfrontend.herokuapp.com'}));
+
 app.options('*', cors())
 app.use(express.urlencoded({extended: false}));
+
 app.use(express.static(path.join(__dirname, '../dist/storeappfrontend')));
 app.use('/app', express.static(path.join(__dirname, '../dist/storeappfrontend')));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
 app.use('/items', usersRouter);
 
 
@@ -48,22 +41,3 @@ var server = http.createServer(app);
 server.listen(port, () => {
   console.log('server running on port ' + port);
 });
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
